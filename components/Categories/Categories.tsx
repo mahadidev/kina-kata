3594f026aee62fb770088f0a8c5fb6810168d12b
Mahadi Dev <mahadi.dev.pm@gmail.com>
2022-09-25 04:38:00 +0600

    Update Promo Card

--- components/Categories/Categories.tsx
+++ components/Categories/Categories.tsx
@@ -1,31 +1,26 @@
-import React from "react";
-import { Category, Button } from "../index";
+import React from 'react';
+import { Button, Category } from '../index';
 
 const Categories = (categories: any) => {
-  return (
-    <div className="pt-16 pb-8">
-      <div className="container m-auto">
-        <h1 className="text-3xl">
-          Popular <span className="text-primary">Category.</span>
-        </h1>
-        <div className="flex flex-wrap justify-center py-4">
-          {categories?.categories
-            ?.slice(0, 14)
-            .map((category: any, i: number) => (
-              <Category key={i} {...category} />
-            ))}
-        </div>
-        <Button
-          href="categories"
-          animation={false}
-          className="underline"
-          type="outline"
-        >
-          View All
-        </Button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="pt-16 pb-8">
+			<div className="container m-auto">
+				<h1 className="text-3xl">
+					Popular <span className="text-primary">Category.</span>
+				</h1>
+				<div className="flex flex-wrap justify-center py-4">
+					{categories?.categories
+						?.slice(0, 14)
+						.map((category: any, i: number) => (
+							<Category key={i} {...category} />
+						))}
+				</div>
+				<Button href="categories" className="underline" type="outline">
+					View All
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Categories;
